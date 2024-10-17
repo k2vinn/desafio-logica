1dc764c73b9c7a681b78737395942707a5709770
k2vinn <kevinfreires11@gmail.com>
2024-10-17 16:29:17 -0300

    projeto finalizado

--- DESAFIO PROJETOS/Desafio-felipao/index.js
+++ DESAFIO PROJETOS/Desafio-felipao/index.js
@@ -1,19 +1,30 @@
-let Heroi = "Mario"
+let Heroi = "Sub-zero"
 let XP = 0
 let nivel = ""
 
-switch (XP) {
-    case (XP<1000):
-        nivel = "Ferro"
-        break
-
-    case (XP>1001 && XP<2000):
-        nivel = "Bronze"
-        break
-
-    case (XP>2001 && XP<5000):
-        nivel = "Prata"
-        break
+if(XP<1000){
+    nivel = "Ferro"
+}
+else if(XP>1001 && XP<2000){
+    nivel = "Bronze"
+}
+else if(XP>2001 && XP<5000){
+    nivel = "Prata"
+}
+else if(XP>5001 && XP<7000){
+    nivel = "Ouro"
+}
+else if(XP>7001 && XP<8000){
+    nivel = "Platina"
+}
+else if(XP>8001 && XP<9000){
+    nivel = "Ascendente"
+}
+else if(XP>9001 && XP<10000){
+    nivel = "Imortal"
+}
+else {
+    nivel = "Radiante"
 }
 
 console.log("O Herói de nome " + Heroi + " está no nível de " + nivel)
