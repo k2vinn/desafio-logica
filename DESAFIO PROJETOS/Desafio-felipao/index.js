3f0b12a22d415f4e3dfc078b6c1c801393f449bd
k2vinn <kevinfreires11@gmail.com>
2024-10-17 16:10:48 -0300

    inicio desafio

--- DESAFIO PROJETOS/Desafio-felipao/index.js
+++ DESAFIO PROJETOS/Desafio-felipao/index.js
@@ -1,1 +1,19 @@
-console.log("Hello Word")
+let Heroi = "Mario"
+let XP = 0
+let nivel = ""
+
+switch (XP) {
+    case (XP<1000):
+        nivel = "Ferro"
+        break
+
+    case (XP>1001 && XP<2000):
+        nivel = "Bronze"
+        break
+
+    case (XP>2001 && XP<5000):
+        nivel = "Prata"
+        break
+}
+
+console.log("O Herói de nome " + Heroi + " está no nível de " + nivel)
