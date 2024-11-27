2b071a65549474c742dd21551e1c8dd10cabac10
k2vinn <kevinfreires11@gmail.com>
2024-11-27 16:59:28 -0300

    game

--- Ri_Happy_Projetos/Projetos/Detona_Ralph/src/scripts/engine.js
+++ Ri_Happy_Projetos/Projetos/Detona_Ralph/src/scripts/engine.js
@@ -28,10 +28,14 @@ function countDown(){
     }
 }
 
-function playSound(audioName){
-    let audio = new Audio(`./src/audios/${audioName}.m4a`);
-    audio.volume = 0.2;
-    audio.play();
+const hitAudio = new Audio("./src/audios/hit.mp3");
+hitAudio.volume = 0.2;
+
+function playSound(audio) {
+    audio.currentTime = 0;
+    audio.play().catch(error => {
+        console.error("Erro ao tentar reproduzir o áudio:", error);
+    });
 }
 
 function randomSquare(){
@@ -52,14 +56,16 @@ function addListenerHitBox(){
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
-                playSound(hit);
+                playSound(hitAudio);
             }
         })
     });
 }
 
 function init() {
-    addListenerHitBox();
+    document.addEventListener("DOMContentLoaded", () => {
+        addListenerHitBox();
+    });
 }
 
 init();
